c871ff51ff216a7922daa97d4e317f5f1521293b
Kosalvireak Khoeun <virakvary@gmail.com>
2022-07-16 21:57:24 +0700

    add education section

--- scripts.js
+++ scripts.js
@@ -9,18 +9,19 @@ function swapImage2() {
     const div = document.querySelector('body')
     const style = window.getComputedStyle(div, false)
     const bac_grou = style.backgroundImage.slice(4, -1).replace(/"/g, "");
-    globalThis.bac_img = "image2.jpg";
+    globalThis.bac_img === "image2.jpg";
 
     document.body.style.backgroundImage = "url(" + img + ")";
     document.getElementById('pic2').src = bac_grou;
 
 }
-newimg = bac_img;
+
 function swapImage3() {
     var img = document.getElementById('pic3').src;
     const div = document.querySelector('body')
     const style = window.getComputedStyle(div, false)
     const bac_grou = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+    globalThis.bac_img === "image3.jpg";
 
     document.body.style.backgroundImage = "url(" + img + ")";
     document.getElementById('pic3').src = bac_grou;
@@ -31,6 +32,7 @@ function swapImage4() {
     const div = document.querySelector('body')
     const style = window.getComputedStyle(div, false)
     const bac_grou = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+    globalThis.bac_img === "image4.jpg";
 
     document.body.style.backgroundImage = "url(" + img + ")";
     document.getElementById('pic4').src = bac_grou;
@@ -41,6 +43,7 @@ function swapImage5() {
     const div = document.querySelector('body')
     const style = window.getComputedStyle(div, false)
     const bac_grou = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+    globalThis.bac_img === "image5.jpg";
 
     document.body.style.backgroundImage = "url(" + img + ")";
     document.getElementById('pic5').src = bac_grou;
@@ -51,6 +54,7 @@ function swapImage6() {
     const div = document.querySelector('body')
     const style = window.getComputedStyle(div, false)
     const bac_grou = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+    globalThis.bac_img === "image6.jpg";
 
     document.body.style.backgroundImage = "url(" + img + ")";
     document.getElementById('pic6').src = bac_grou;
@@ -61,6 +65,7 @@ function swapImage7() {
     const div = document.querySelector('body')
     const style = window.getComputedStyle(div, false)
     const bac_grou = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+    globalThis.bac_img === "image7.jpg";
 
     document.body.style.backgroundImage = "url(" + img + ")";
     document.getElementById('pic7').src = bac_grou;
@@ -71,12 +76,13 @@ function swapImage8() {
     const div = document.querySelector('body')
     const style = window.getComputedStyle(div, false)
     const bac_grou = style.backgroundImage.slice(4, -1).replace(/"/g, "");
+    globalThis.bac_img === "image8.jpg";
 
-        
+    document.body.style.backgroundImage = "url(" + img + ")";
     document.getElementById('pic8').src = bac_grou;
 
 }
 function About_Me(){
-    document.body.style.backgroundImage = "url(" + newimg + ")";
+    document.body.style.backgroundImage = "url(" + bac_img + ")";
 
 }
